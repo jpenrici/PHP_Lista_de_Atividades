1654fa7396f354b02a8886391eb57cd717cea998
Juan Pablo Enrici <jpenrici@gmail.com>
2024-04-28 18:57:19 -0300

    Escopo recomendado para as Atividades.

---
+++ Atividades_Logica/Aula01/Atividade_01/script.js
@@ -0,0 +1,14 @@
+/*
+1. Aqui você comenta a atividade!
+*/
+
+// Declaração de variáveis com Entrada de Dados.
+let numero_01 = window.prompt("Digite o primeiro número:");
+let numero_02 = window.prompt("Digite o segundo número:");
+
+// Declaração de variáveis e operações.
+let resultado = parseFloat(numero_01) + parseFloat(numero_02);
+let mensagem = numero_01 + " + " + numero_02 + " = " + resultado;
+
+// Saída de Dados no Alert do Navegador.
+window.alert(mensagem);
