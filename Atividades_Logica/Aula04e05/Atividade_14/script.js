8d1bdcbe04f297b266b19437fa4628bb96c874b9
Juan Pablo Enrici <jpenrici@gmail.com>
2024-05-03 17:23:32 -0300

    Sugestão de correção feitas pelos alunos.

--- Atividades_Logica/Aula04e05/Atividade_14/script.js
+++ Atividades_Logica/Aula04e05/Atividade_14/script.js
@@ -2,17 +2,17 @@
 14. Construa uma página onde o usuário digitará quatro notas escolares e o programa irá
 calcular a média e, caso seja menor que 6, exibirá na tela: “Aluno Reprovado”. Caso
 seja maior ou igual a 6 exibirá na tela: “Aluno Aprovado”.
-15. Construa uma página/programa onde o usuário digitará t
 */
 
 let nota_01 = window.prompt("Digite a primeira nota:");
 let nota_02 = window.prompt("Digite a segunda nota:");
 let nota_03 = window.prompt("Digite a terceira nota:");
 let nota_04 = window.prompt("Digite a quarta nota:");
-let media = (nota_01 + nota_02 + nota_03 + nota_04) / 4;
+let media = (parseFloat(nota_01) + parseFloat(nota_02) + parseFloat(nota_03) + parseFloat(nota_04)) / 4;
 let mensagem = "Aluno Aprovado";
 
-if (media < 6) {
+console.log("Média: ", media); // Observar na Ferramenta do Desevolvedor do Navegador.
+if (parseFloat(media) < 6) {
     mensagem = "Aluno Reprovado";
 }
 
--- Atividades_Logica/Aula04e05/Atividade_16/script.js
+++ Atividades_Logica/Aula04e05/Atividade_16/script.js
@@ -2,11 +2,22 @@
 16. Construa uma página onde o usuário digitará o seu peso e a sua altura e o programa
 irá calcular o IMC (peso/altura). Caso o IMC seja maior que 25 exibirá, na tela, “Acima
 do Peso Ideal”. Caso contrário, “Peso OK”.
+
+Fonte: https://pt.wikipedia.org/wiki/%C3%8Dndice_de_massa_corporal
+
+    Resultado               Situação
+    Abaixo de 17	        Muito abaixo do peso
+    Entre 17 e 18,49	    Abaixo do peso
+    Entre 18,50 e 24,99	    Peso normal
+    Entre 25 e 29,99	    Acima do peso
+    Entre 30 e 34,99	    Obesidade I
+    Entre 35 e 39,99	    Obesidade II (severa)
+    Acima de 40	            Obesidade III (mórbida)
 */
 
-let peso = window.prompt("Digite o seu peso:");
-let altura = window.prompt("Digite a sua altura:");
-let IMC = peso / altura;
+let peso = window.prompt("Digite o seu peso (Kg):");
+let altura = window.prompt("Digite a sua altura (m):");
+let IMC = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));
 
 let mensagem = "Peso OK";
 if (IMC > 25) {
