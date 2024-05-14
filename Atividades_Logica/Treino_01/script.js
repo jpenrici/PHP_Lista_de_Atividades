126f9b222d84f5d7a4afc47a9936e20537bfd887
Juan Pablo Enrici <jpenrici@gmail.com>
2024-05-14 09:28:14 -0300

    Treino

---
+++ Atividades_Logica/Treino_01/script.js
@@ -0,0 +1,108 @@
+/*
+    Dicas:
+
+    1) Leia atentamente todas as linhas de código, antes de iniciar.
+    3) Leia cada linha comentada, escreva o código abaixo e teste ou observe.
+
+*/
+
+///////////////////////////////////////////////////////////////
+/* REVISÃO DE VARIÁVEIS, CONSTANTES, EXPRESSÕES E OPERADORES */
+///////////////////////////////////////////////////////////////
+
+// Declare uma variável chamada "numero" e atribua o valor dez.
+
+// Declare uma variável chamada "numReal" e atribua o valor 10,50
+
+// Declare uma variável chamada "soma" e atribua a expressão aritmética de adição, somando "numero" com "numReal"
+
+// Declare uma variável chamada "divisao" e atribua a expressão "10 / 0"
+
+// Declare uma variável chamada "caracter" e atribua o valor "A"
+
+// Declare uma variável chamada "espaco" e atribua o valor " "
+
+// Declare uma variável chamada "caracterEspecial_quebraDeLinha" e atribua o valor "\n"
+
+// Declare uma variável chamada "caracterEspecial_tabulacao" e atribua o valor "\t"
+
+// Declare uma variável chamada "texto" e atribua o valor "isso é uma string"
+
+// Declare uma constante chamada "TEXTO" e atribua o valor "isso é uma constante"
+
+// Declare uma variável chamada "textoConcatenado1" e atribua a concatenação (Operador +) da string "texto" com "espaco" e "TEXTO"
+
+// Declare uma variável chamada "textoConcatenado2" e atribua a concatenação da string "texto" com "caracterEspecial_quebraDeLinha" e "TEXTO"
+
+// Declare uma variável chamada "textoConcatenado3" e atribua a concatenação da string "texto" com "caracterEspecial_tabulacao" e "TEXTO"
+
+// Atribua a variável "TEXTO" o valor "o que acontece se atribuir novo valor a constante?"
+
+// Declare uma variável chamada "_varValida01" e atribua o valor Verdadeiro em JavaScript
+
+// Declare uma variável chamada "$varValida02" e atribua o valor Falso em JavaScript
+
+// Atribua a variável "$varValida02" o valor 1001
+
+// Use a função interna do JavaScript chamada console.log() para exibir os resultados dos dados acima no terminal.
+
+/////////////////////////////////////////////
+/* REVISÃO DE CONVERSÃO DE TIPOS (CASTING) */
+/////////////////////////////////////////////
+
+// Declare a variável chamada "palavra" e atribua o valor tipo string "10"
+
+// Declare a variável chamada "valor" e atribua a conversão da variável "palavra" para inteiro.
+
+// Declare a variável chamada "valor" e atribua a conversão da variável "palavra" para real.
+
+//////////////////////////////////////
+/* REVISÃO DE ESTRUTURA CONDICIONAL */
+//////////////////////////////////////
+
+// Construa uma tomada de decisão usando "Se" para exibir no terminal se a variável "numero" "É igual a 1000"
+
+// Construa uma tomada de decisão usando "Se e Senão" para exibir no terminal se a variável "numero" "É par" ou "É ímpar"
+
+// Construa uma tomada de decisão usando "Se Senão, Se Senão ..." para exibir no terminal se a variável "numero" "É divisível por 5" ou "Não é divisível por 5",
+// caso positivo, verificar se "É par" e exibir no terminal "É par"
+
+// Declare a variável chamada "opcao" e atribua o valor 2
+
+// Faça um teste pra exibir no terminal "Opção 1", "Opção 2", etc. Utilize o comando "switch".
+
+//////////////////////////
+/* REVISÃO DE REPETIÇÃO */
+//////////////////////////
+
+// Use o comando(instrução) "for" para exibir no terminal "Este é o índice número X", onde X corresponde a variável de contagem da estrutura de repetição.
+
+// Declare uma variável chamada "mensagem" e atribua um valor do tipo string vazio
+
+// Declare uma variável chamada "contador" e atrubua o valor zero
+
+/*
+ Use o comando(instrução) "while" para exibir no terminal "Este é o índice número X", onde X corresponde a variável "contador".
+ Faça com que a variável "mensagem" armazene a sequência da contagem.
+ Faça com que o "loop" pare quando contador atingir o valor dez.
+ Exiba no terminal o texto da variável "mensagem"
+*/ 
+
+/*
+ Use o comando(instrução) "do while" para exibir no terminal "Este é o índice número X", onde X corresponde a variável "contador".
+ Faça com que a variável "mensagem" armazene a sequência da contagem.
+ Faça com que o "loop" pare quando contador atingir o valor negativo cinco.
+ Exiba no terminal o texto da variável "mensagem"
+*/ 
+
+//////////////////////////////
+/* REVISÃO DE ENTRADA/SAÍDA */
+//////////////////////////////
+
+// Explique o que acontece com o comando(instrução) abaixo se forem descomentados.
+
+// window.prompt("Digite o seu nome:");
+// endereco = window.prompt("Digite o seu enderço:")
+
+// window.alert("Hello!");
+// window.alert("endereco");
