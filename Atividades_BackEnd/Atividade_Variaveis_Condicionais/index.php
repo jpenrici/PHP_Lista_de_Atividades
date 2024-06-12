3a20a09896e3f9636e7b174abfb4e753906f603d
Juan Pablo Enrici <jpenrici@gmail.com>
2024-06-12 15:55:59 -0300

    Atualizando Atividades.

---
+++ Atividades_BackEnd/Atividade_Variaveis_Condicionais/index.php
@@ -0,0 +1,189 @@
+<!--
+        Dicas:
+
+            1) Leia atentamente todas as linhas de código, antes de iniciar.
+            3) Leia cada linha comentada, escreva o código abaixo e teste (observe).
+
+        Para está atividade é necessário um ambiente de Localhost.
+
+        Fontes úteis:
+            https://www.php.net/docs.php
+            https://www.w3schools.com/php/default.asp
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade Variáveis</title>
+</head>
+
+<body>
+    <!-- Desenvolva os códigos para exibir no navegador as respostas. -->
+
+    <h1>Atividade Variáveis</h1>
+    <hr>
+
+    <!--
+    1.
+        Declare a variável Resultado.
+        Atribua no Resultado o valor do tipo string: "<p>Olá Desenvolvedor PHP!</p>".
+        Exiba o resultado neste HTML.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    2.
+        Declare as variáveis Nome, Resultado.
+        Atribua em Nome: o seu nome.
+        Atribua no Resultado: "Bem-vindo! Olá, Nome!", usando a variável Nome.
+        Exiba o resultado neste HTML.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    3.
+        Declare as variáveis Valor, Desconto, Resultado.
+        Atribua no Resultado o cálculo que aplique o desconto no valor. Crie valores para teste.
+        Exiba o resultado na forma de parágrafo neste HTML.
+        Exiba o resultado na forma literal. Por exemplo, "O valor igual a 10 é 10% do valor 100.".
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    4.
+        Declare as variáveis Velocidade, Resultado.
+        Atribua no Resultado o cálculo da conversão de m/s para km/h, usando a Velocidade.
+        Exiba o resultado na forma de parágrafo e formatação negrito para o valor numérico com a unidade.
+        Exiba o resultado como informação atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    5.
+        Declare as variáveis Comprimento, Largura, Altura, Volume (Resultado).
+        Atribua no Resultado o cálculo do volume de uma caixa retangular.
+        Exiba o resultado na forma de parágrafo e formatação itálico.
+        Exiba o resultado como informação atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    6.
+        Declare as variáveis Comprimento, Largura, Altura, Volume (Resultado).
+        Atribua no Resultado o cálculo do volume de uma pirâmide.
+        Exiba o resultado na forma de parágrafo e formatação itálico e cor da fonte azul.
+        Exiba o resultado como informação atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    7.
+        Declare a variável Resultado.
+        Atribua no Resultado o valor do tipo string para construir uma lista ordenada,
+        contendo as 20 linguagens de programação mais utilizadas segundo o site https://www.tiobe.com/.
+        Exiba o resultado neste HTML.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    8.
+        Declare a variável Resultado.
+        Atribua no Resultado o valor do tipo string para construir uma tabela
+        para listar 5 marcas de liquidificador, com nome, fabricante, preço.
+        Exiba o resultado neste HTML.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+    <h1>Atividade Estrutura Condicional</h1>
+    <hr>
+
+    <!--
+    9.
+        Declare as variáveis Nota1, Nota2, Nota3, Nota4, Nota5 e Média (Resultado).
+        Atribua no Resultado o cálculo da média das notas.
+        Exiba o resultado na forma de parágrafo, indicando se o aluno está reprovado (nota menor que seis) ou aprovado.
+        Exiba o resultado como informação atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    10.
+        Declare as variáveis Altura, Peso e IMC ou Resultado.
+        Atribua no Resultado o cálculo do IMC.
+        Crie valores para teste.
+        Exiba o resultado na forma de parágrafo, formatando a resposta em cores conforme a classificação IMC.
+        Exiba o resultado como informação atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    11.
+        Declare as variáveis Escolha, Resultado.
+        Construa uma estrutura de escolha de opção para responder conforme o tipo de erro do servidor uma mensagem.
+        Crie situações teste. Pesquise os principais tipos de códigos de erro HTTP e utilize pelo menos cinco tipos.
+        Exiba o resultado neste HTML.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+    <h1>Atividade Conversão de Tipos</h1>
+    <hr>  
+    
+    <!--
+    12.
+        Declare as variáveis ValorInteiro, ValorFracionado, ValorTexto, ValorLogico, Resultado.
+        Atribua valores correspondentes ao nome da variável.
+        Atribua ao Resultado valores convertidos e exiba.
+        Faça isso para todas as possibilidades: Inteiro pra Fração, Fração pra Texto, etc.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>    
+
+</body>
+
+</html>
