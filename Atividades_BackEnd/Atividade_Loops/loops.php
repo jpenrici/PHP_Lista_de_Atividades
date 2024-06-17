1794e94656cf38b29a3fcd123c999e0afb86609b
Juan Pablo Enrici <jpenrici@gmail.com>
2024-06-17 18:51:17 -0300

    Atividade de Estrutura de Repetição.

---
+++ Atividades_BackEnd/Atividade_Loops/loops.php
@@ -0,0 +1,179 @@
+<!--
+        Dicas:
+
+            1) Leia atentamente todas as linhas de código, antes de iniciar.
+            3) Leia cada linha comentada, escreva o código abaixo e teste (observe).
+
+        Para está atividade é necessário um ambiente de Localhost.
+
+        Fontes úteis:
+            https://www.php.net/docs.php
+            https://www.w3schools.com/php/default.asp
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade de BackEnd</title>
+</head>
+
+<body>
+    <!-- Desenvolva os códigos para exibir no navegador as respostas. -->
+
+    <h1>Atividade de Estrutura de Repetição</h1>
+    <hr>
+
+    <h2>Para Até (For)</h2>
+
+    <hr>
+
+    <!--
+    1.
+        Utilize o comando For em estrutura simples (único loop) para exibir o resultado
+        da contagem de -50 a 50. Exiba cada valor inteiro, um a um.
+        Exiba o resultado neste HTML em formato amigável, em texto explicativo!
+        Por exemplo: "1º : -50 , 2º : -49".
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <!--
+    2.
+        Utilize o comando For em estrutura simples (único loop) para exibir o resultado
+        dos 15 primeiros números da sequência que segue está lógica: 3, 6, 18, 4, 8, 12, ... .
+        Exiba o resultado neste HTML em formato amigável, organizado!
+        Mostre se possível como os cálculos estão sendo executados.
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    3.
+        Utilize o comando For em estrutura simples (único loop) para construir uma saída usando asteriscos,
+        que irá "desenhar" o exemplo abaixo. Faça de forma variável, ou seja, podendo alterar as entradas.
+        Aplique estilos de cor para tornar o resultado mais atrativo! Use a criatividade!
+        Exiba o resultado neste HTML!
+
+        Exemplo: AlturaDaCopa = 5, AlturaDoTronco = 2.
+
+                       *       
+                      ***  
+                     *****
+                    *******
+                   ********* 
+                      ***
+                      ***
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    4.
+        Utilize o comando For em estrutura dupla (For dentro de For) para exibir a construção
+        de uma Tabela sem Cabeçalho contendo em cada célula o número da linha e o número da coluna.
+        Utilize a tag Table com bordas para melhor visualização.
+        Faça de forma variável, ou seja, podendo alterar as entradas.
+        Exiba o resultado neste HTML em formato amigável, atrativa!
+
+        Exemplo: Construção de uma Tabela de 2 linhas por 2 colunas.
+
+                 +-------------------+-------------------+
+                 | Linha 1, Coluna 1 | Linha 1, Coluna 2 |
+                 +-------------------+-------------------+
+                 | Linha 2, Coluna 1 | Linha 2, Coluna 2 |
+                 +-------------------+-------------------+           
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?> 
+
+    <hr>
+
+    <!--
+    5.
+        Utilize o comando For em estrutura dupla (For dentro de For) para exibir a construção
+        de uma Tabela gerando um tabuleiro de Xadrez. 
+        Faça de forma variável, ou seja, podendo alterar as entradas.
+        Aplique estilos de cor para tornar o resultado mais atrativo! Use a criatividade!
+        Exiba o resultado neste HTML!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <h2>Enquanto (While)</h2>
+
+    <hr>
+
+    <!--
+    6.
+        Utilize o comando While para exibir os 30 números divisíveis por 3 e por 4 ao mesmo tempo.
+        Exiba o resultado neste HTML em formato amigável, atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    7.
+        Utilize o comando While para exibir a conversão da temperatura de Grau Celsius para Grau Fahrenheit.
+        Exiba 20 conversões de temperatura como resultado neste HTML em formato amigável, atrativo!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <!--
+    8.
+        Utilize o comando While para exibir a sequência de 100 caracteres seguindo a lógica: \o/_o_/o\_\o/_o_/o\_
+        Exiba o resultado neste HTML!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+
+    <h2>Faça Até (Do While)</h2>   
+
+    <hr>
+    
+    <!--
+    9.
+        Utilize o comando Do While para exibir os 25 números divisíveis por 2, por 4 e por 6 ao mesmo tempo.
+        Exiba o resultado neste HTML em formato amigável, atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+    <hr>
+    
+    <!--
+    10.
+        Utilize o comando Do While para exibir a conversão de Metros quadrados para Centímetros quadrados.
+        Exiba 25 resultados neste HTML em formato amigável, atrativa!
+    -->
+    <?php
+        // Escreva aqui o código.
+    ?>
+
+</body>
+
+</html>
