748daef63cfc22c3130b54701b1daa8e87890cdc
Juan Pablo Enrici <jpenrici@gmail.com>
2024-07-01 10:22:34 -0300

    Atualização.

---
+++ Template/WebSite/database/php/crud_user.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once 'db_functions.php';
+
+function create_user($database, $username, $email, $password1, $password2)
+{
+    $result = null;
+
+    // Checar entradas.
+    if (is_null_or_empty($database)) {
+        return false;
+    }
+
+    if (is_null_or_empty([$username, $email, $password1, $password2])) {
+        return false;
+    }
+
+    if ($password1 != $password2) {
+        return false;
+    }
+
+    // Conectar.
+    $pdo = connect($database['hostname'], $database['dbname'], $database['user'], $database['password']);
+
+    // Procurar.
+    $sql = "SELECT * FROM `user` WHERE BINARY `username` = '" . $username . "';";
+    $result = command($pdo, $sql);
+    if ($result) {
+        $count = $result->rowCount();
+        if ($count > 0) {
+            // echo "Found " . $count . " entries for " . $username . "! New user has not been created!" . PHP_EOL;
+            return false;
+        }
+    }
+
+    // Criar.
+    $sql = "INSERT INTO `user` (`id`, `username`, `email`, `password`, `administrator`, `createdAt`, `updatedAt`) ";
+    $sql .= "VALUES (NULL, '" . $username . "', '" . $email . "', '" . $password1 . "', '0', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);";
+    $result = command($pdo, $sql);
+
+    // Desconectar.
+    $pdo = null;
+
+    return !is_null($result);
+}
+
+function read_user_by_username($database, $username)
+{
+    $result = null;
+
+    // Checar entradas.
+    if (is_null_or_empty($database)) {
+        return null;
+    }
+
+    if (is_null_or_empty($username)) {
+        return null;
+    }
+
+    // Conectar.
+    $pdo = connect($database['hostname'], $database['dbname'], $database['user'], $database['password']);
+
+    // Procurar.
+    $sql = "SELECT * FROM `user` WHERE BINARY `username` = '" . $username . "';";
+    $result = command($pdo, $sql);
+
+    // Desconectar.
+    $pdo = null;
+
+    return $result;
+}
+
+function update_user_by_id($database, $id, $username, $email, $password)
+{
+    $result = null;
+
+    // Checar entradas.
+    if (is_null_or_empty($database)) {
+        return false;
+    }
+
+    if (is_null_or_empty($id)) {
+        return false;
+    }
+
+    // Conectar.
+    $pdo = connect($database['hostname'], $database['dbname'], $database['user'], $database['password']);
+
+    // Procurar.
+    $result = find_by_id($pdo, 'user', $id);
+    if ($result) {
+        // Atualizar.
+        $data = $result->fetch(PDO::FETCH_ASSOC);
+        $new_username = (empty($username) || is_null($username)) ? $data['username'] : $username;
+        $new_email = (empty($email) || is_null($email)) ? $data['email'] : $email;
+        $new_password = (empty($password) || is_null($password)) ? $data['password'] : $password;
+        $updateAt = date("Y-m-d H:i:s");
+        // echo "Values: " . $id . ", " . $new_username . ", " . $new_email . ", " . $new_password . ", " . $updateAt . PHP_EOL;
+
+        $sql = "UPDATE `user` SET `username` = '" . $new_username . "', `email` = '" . $new_email . "', ";
+        $sql .= "`password` = '" . $new_password . "', `updatedAt` = '" . $updateAt . "' WHERE `user`.`id` = " . $id . ";";
+        $result = command($pdo, $sql);
+    }
+
+    // Desconectar.
+    $pdo = null;
+
+    return !is_null($result);
+}
+
+function delete_user_by_id($database, $id)
+{
+    return delete_by_id($database['hostname'], $database['dbname'], $database['user'], $database['password'], 'user', $id);
+}
+
+function list_all_users($database)
+{
+    return list_all_itens($database['hostname'], $database['dbname'], $database['user'], $database['password'], 'user');
+}
+
+// crud_user.php
---
+++ Template/WebSite/database/php/test_crud_user.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../config/config.php";
+// var_dump($database);
+
+require_once "crud_user.php";
+
+$username = "user1";
+
+$result = create_user($database, $username, "$username@provedor1.com", "123", "123");
+echo "Criar usuário ... " . ($result ? "ok" : "ignorado") . PHP_EOL;
+
+$result = read_user_by_username($database, $username);
+echo "Encontrado " . ($result->rowCount()) . " registros para " . $username . "!" . PHP_EOL;
+
+$result = update_user_by_id($database, '10', "", "newemail@provedor1.com", "");
+echo "Atualizar id ... " . ($result ? "ok" : "ignorado") . PHP_EOL;
+
+$result = delete_user_by_id($database, '10');
+echo "Deletar id ... " . ($result ? "ok" : "ignorado") . PHP_EOL;
+
+$result = list_all_users($database);
+echo "Encontrado " . ($result->rowCount()) . " registros!" . PHP_EOL;
+
+// test_crud_user.php
